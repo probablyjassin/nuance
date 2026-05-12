511afae8c64fd9f69e96ceac38fd64482d661954
Jässin Aouani <aouanijassin@gmail.com>
2026-05-12 12:14:20 +0200

    seperate folder for bottom tabs

--- app/src/main/java/com/jassin/customdrome/AppNavigation.kt
+++ app/src/main/java/com/jassin/customdrome/AppNavigation.kt
@@ -22,6 +22,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.jassin.customdrome.tabs.Playlists
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
--- app/src/main/java/com/jassin/customdrome/tabs/Playlists.kt
+++ app/src/main/java/com/jassin/customdrome/tabs/Playlists.kt
@@ -1,4 +1,4 @@
-package com.jassin.customdrome
+package com.jassin.customdrome.tabs
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
