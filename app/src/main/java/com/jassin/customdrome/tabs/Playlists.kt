14ca6e76eb5b527160f47ca1d978825235b62eb7
probablyjassin <aouanijassin@gmail.com>
2026-05-13 16:11:44 +0200

    small layout improvements

--- app/src/main/java/com/jassin/customdrome/tabs/Playlists.kt
+++ app/src/main/java/com/jassin/customdrome/tabs/Playlists.kt
@@ -1,25 +1,21 @@
 package com.jassin.customdrome.tabs
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun Playlists() {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxWidth()
-                .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+    Box(
+        modifier = Modifier.fillMaxSize().safeDrawingPadding(),
+        contentAlignment = Alignment.Center,
     ) {
-        Text("Playlists Screen")
+        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("Playlists Screen")
+        }
     }
 }
