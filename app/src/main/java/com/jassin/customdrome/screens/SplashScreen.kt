6d219e34bf2aa9e9eb5c97841d22068738aad0db
probablyjassin <123012687+probablyjassin@users.noreply.github.com>
2026-05-16 20:00:05 +0200

    swap spash screen text for a loading spinner

--- app/src/main/java/com/jassin/customdrome/screens/SplashScreen.kt
+++ app/src/main/java/com/jassin/customdrome/screens/SplashScreen.kt
@@ -2,9 +2,12 @@ package com.jassin.customdrome.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.jassin.customdrome.UserPreferences
 import com.jassin.customdrome.data.api.NavidromeApiClient
@@ -75,9 +78,9 @@ fun SplashScreen(
         }
     }
 
-    // screen content (barely visible)
-    Box {
-        Text("Checking authentication...")
+    // screen content (only briefly visible)
+    Box(Modifier.Companion.fillMaxSize(), contentAlignment = Alignment.Companion.Center) {
+        CircularProgressIndicator()
     }
 }
 
